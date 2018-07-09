15ea2037c30c00773be02fffd86fbb2f3f088cef
Nikita Voloboev <nikita.voloboev@gmail.com>
2018-07-09 07:50:36 +0200

    add README.md

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ The idea is to make a small language with first class support for variables, sti
 
 The script is a modified version of a [ruby script](https://github.com/pqrs-org/KE-complex_modifications/blob/master/src/json/personal_tekezo_launcher_mode_v4.json.rb) by [Tekezo](https://github.com/tekezo).
 
+The [included script](karabiner-generate.rb) contains my own private keyboard configuration. To generate it and add it to Karabiner Elements, run this command:
+`ruby karabiner-generate.rb | pbcopy`
+
+This will put Karabiner JSON of all the rules in your clipboard. You can then paste it inside Karabiner in-between `rules []` block.
+
 ## Related
 - [Karabiner JSON Spec](https://pqrs.org/osx/karabiner/json.html)
 
--- karabiner-generate.rb
+++ karabiner-generate.rb
@@ -5,8 +5,6 @@ PARAMETERS = {
   :simultaneous_threshold_milliseconds => 500,
 }.freeze
 
-############################################################
-
 require 'json'
 require_relative 'karabiner.rb'
 
@@ -19,60 +17,30 @@ def alfred(trigger, workflow)
 end
 
 def main
-  sticky_q()
+  sticky_i()
 end
+
+# TODO: alphabetize all manipulators based on trigger key. can use vim magic for it
+
+# TODO: add HS calls. do it in background
+# hammerspoon://openConsole -> opens console
+# hammerspoon://showTime -> shows time
+# hammerspoon://reloadConfig -> reloads HS
+# hammerspoon://test -> tests some HS func
+
+# SMALL BINDS
 
 def swapkeys()
     puts JSON.pretty_generate(
         'description' => 'swap keys',
         'manipulators' => [
-            swapkey("semicolon", "semicolon", nil, ["caps_lock"], ["left_shift"]),
+            swapkey("semicolon", "semicolon", nil, ["caps_lock"], ["left_shift"]), # ; -> :
             swapkey("semicolon", "semicolon", ["shift"], ['any'], nil), # shift + : -> ;
         ].flatten,
   )
-
 end
 
-def sticky_w()
-  puts JSON.pretty_generate(
-        'description' => 'gen: sticky w - apps',
-        'manipulators' => [
-            shell('w', 'k', km("open: safari")),
-            shell('w', 'i', km("open: chrome")),
-            shell('w', 'n', km("open: bee")),
-            shell('w', 'h', km("open: xcode")),
-            shell('w', 't', km("open: console")),
-            shell('w', 'period', km("open: karabiner elements")),
-            shell('w', 'comma', km("open: spotify")),
-            shell('w', 'v', km("open: mindnode")),
-            shell('w', 'b', km("open: bettertouchtool")),
-            shell('w', 'r', km("open: fantastical")),
-            shell('w', 'e', km("open: trello")),
-            shell('w', 'g', km("open: snippetslab")),
-            shell('w', 'f', km("open: 2do")),
-            shell('w', 'j', km("open: iterm")),
-            shell('w', 'l', km("open: sublime text")),
-            shell('w', 'semicolon', km("open: vs code")),
-            shell('w', 'o', km("open: keyboard maestro")),
-            shell('w', 'p', alfred('google','net.deanishe.alfred-searchio.old')),
-        ].flatten,
-  )
-end
-
-def sticky_s()
-  puts JSON.pretty_generate(
-        'description' => 'gen: sticky s - essential',
-        'manipulators' => [
-          shell('s', 'f', 'return_or_enter'),
-          shell('s', 'j', 'down_arrow'),
-          shell('s', 'k', 'up_arrow'),
-          shell('s', 'h', 'left_arrow'),
-          shell('s', 'l', 'right_arrow'),
-          shell('s', 'l', 'right_arrow'),
-          shell('s', 'm', 'right_arrow', 'left_command'),
-        ].flatten,
-  )
-end
+# STICKY KEY DEFINITIONS
 
 def sticky_q()
   puts JSON.pretty_generate(
@@ -103,7 +71,189 @@ def sticky_q()
           key("q", "b", "b", ["command", "shift"]),
           key("q", "n", "n", ["command", "shift"]),
           key("q", "m", "m", ["command", "shift"]),
-          key("q", "comma", "comma", ["command", "shift"]),
+          key("q", "1", "1", ["command", "shift"]),
+          key("q", "2", "2", ["command", "shift"]),
+          key("q", "3", "3", ["command", "shift"]),
+          key("q", "4", "4", ["command", "shift"]),
+          key("q", "5", "5", ["command", "shift"]),
+          key("q", "6", "6", ["command", "shift"]),
+          key("q", "7", "7", ["command", "shift"]),
+          key("q", "8", "8", ["command", "shift"]),
+          key("q", "9", "9", ["command", "shift"]),
+          key("q", "spacebar", "x", ["shift", "option", "control"]), # google search selected text
+        ].flatten,
+  )
+end
+
+# TODO: finish
+def sticky_w()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky w - apps',
+        'manipulators' => [
+            shell('w', 'k', km("open: safari")),
+            shell('w', 'i', km("open: chrome")),
+            shell('w', 'n', km("open: bee")),
+            shell('w', 'h', km("open: xcode")),
+            shell('w', 't', km("open: console")),
+            shell('w', 'period', km("open: karabiner elements")),
+            shell('w', 'comma', km("open: spotify")),
+            shell('w', 'v', km("open: mindnode")),
+            shell('w', 'b', km("open: bettertouchtool")),
+            shell('w', 'r', km("open: fantastical")),
+            shell('w', 'e', km("open: trello")),
+            shell('w', 'g', km("open: snippetslab")),
+            shell('w', 'f', km("open: 2do")),
+            shell('w', 'j', km("open: iterm")),
+            shell('w', 'l', km("open: sublime text")),
+            shell('w', 'semicolon', km("open: vs code")),
+            shell('w', 'o', km("open: keyboard maestro")),
+        ].flatten,
+  )
+end
+
+def sticky_e()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky e - cmd',
+        'manipulators' => [
+            key("e", "q", "q", ["command"]),
+            key("e", "w", "w", ["command"]),
+            key("e", "r", "r", ["command"]),
+            key("e", "t", "t", ["command"]),
+            key("e", "y", "y", ["command"]),
+            key("e", "u", "u", ["command"]),
+            key("e", "i", "i", ["command"]),
+            key("e", "o", "o", ["command"]),
+            key("e", "p", "p", ["command"]),
+            key("e", "a", "a", ["command"]),
+            key("e", "s", "s", ["command"]),
+            key("e", "d", "d", ["command"]),
+            key("e", "f", "f", ["command"]),
+            key("e", "g", "g", ["command"]),
+            key("e", "h", "h", ["command"]),
+            key("e", "j", "j", ["command"]),
+            key("e", "k", "k", ["command"]),
+            key("e", "l", "l", ["command"]),
+            key("e", "z", "z", ["command"]),
+            key("e", "x", "x", ["command"]),
+            key("e", "c", "c", ["command"]),
+            key("e", "v", "v", ["command"]),
+            key("e", "b", "b", ["command"]),
+            key("e", "n", "n", ["command"]),
+            key("e", "m", "m", ["command"]),
+            key("e", "comma", "comma", ["command"]),
+            key("e", "period", "period", ["command"]),
+        ].flatten,
+  )
+end
+
+def sticky_r()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky r - apps',
+        'manipulators' => [
+          key('r', 'space', 's', ["shift", "control"]), # web searches with selected text
+          shell('r', '1', km('open: hazel')),
+          shell('r', 'u', km('open: actual')),
+          shell('r', 'l', km('open: ulysses')),
+          shell('semicolon', 'semicolon', km('open: paw')),
+          shell('r', 'slash', km('open: little snitch configuration')),
+          shell('r', 'tab', km('open: flume')),
+          shell('r', 't', km('open: transmission')),
+          shell('r', 'p', km('open: paprika recipe manager')),
+          shell('r', 'b', km('edit: edit keyboard shortcuts')),
+          shell('r', 'comma', km('open: day one')),
+          shell('r', 'm', km('open: marked')),
+          shell('r', 'caps_lock', km('open: magic number')),
+          shell('r', 's', km('open: itunes')),
+          shell('r', 'a', km('open: alfred preferences')),
+          shell('r', 'i', km('open: pixave')),
+          shell('r', 'e', km('open: reeder')),
+          shell('r', 'j', km('open: dictionary')),
+          shell('r', 'h', km('open: hammerspoon')),
+          shell('r', 'k', km('open: pdf expert')),
+          shell('r', 'w', km('open: 1password')),
+          shell('r', 'v', km('open: keychain access')),
+          shell('r', 'n', km('open: timing')),
+          key("r", "o", "f10", ["option"], ["control"]), # open typinator
+        ].flatten,
+  )
+end
+
+def sticky_t()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky t - handy actions',
+        'manipulators' => [
+          shell('t', 'i', km('g: new snippetslab snippet')),
+          shell('t', 'o', km('g: new mindnode file')),
+          shell('t', 'j', km('g: goto km group of current app')),
+          shell('t', 'k', km('g: goto km group of current app from picklist')),
+          key("t", "spacebar", "r", ["shift", "option", "command"]), # google translate selected text
+          shell("t", "period", km('g: stop all macros')),
+          key("t", "s", "9", ["command", "control"]), # little snitch network monitor
+          shell("t", "a", alfred('clean', 'nikivi.clean.folders')),
+          shell("t", "y", alfred('gitUpdate', 'net.deanishe.alfred-git-repos')),
+          shell('t', 'w', km('per: pass')),
+        ].flatten,
+  )
+end
+
+def sticky_y()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky y - websites',
+        'manipulators' => [
+          shell('y', 'b', km('w: brilliant')),
+          shell('y', 'd', km('w: pinboard')),
+          shell('y', 'h', km('w: unsplash')),
+        ].flatten,
+  )
+end
+
+def sticky_u()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky u - websites',
+        'manipulators' => [
+          shell('u', 'e', km('w: repl')),
+          shell('u', 'd', km('w: codepen')),
+          shell('u', 'a', km('w: arxiv')),
+          shell('u', 'l', km('w: bitly')),
+          shell('u', 'x', km('w: dropbox')),
+          shell('u', 'v', km('w: twitch')),
+          shell('u', 'b', km('w: gitbook')),
+          shell('u', 'r', km('w: google drive')),
+          shell('u', 'h', km('w: khan academy')),
+          shell('u', 'spacebar', km('g: open url in safari')),
+          shell('u', 't', km('w: github trending')),
+          shell('u', 'z', km('w: MDN')),
+          shell('u', 'y', km('w: netlify')),
+          shell('u', 'g', km('w: kaggle')),
+          shell('u', 'i', km('w: wakatime')),
+          shell('u', 'w', km('w: AWS')),
+          shell('u', 's', km('w: NPM')),
+        ].flatten,
+  )
+end
+
+# TODO: finish
+def sticky_i()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky ',
+        'manipulators' => [
+            multiple_key("i", "b", 'v', 'o', ["shift"], ['shift'])
+        ].flatten,
+  )
+end
+
+# TODO: fix!
+def sticky_s()
+  puts JSON.pretty_generate(
+        'description' => 'gen: sticky s - essential',
+        'manipulators' => [
+          shell('s', 'f', 'return_or_enter'),
+          shell('s', 'j', 'down_arrow'),
+          shell('s', 'k', 'up_arrow'),
+          shell('s', 'h', 'left_arrow'),
+          shell('s', 'l', 'right_arrow'),
+          shell('s', 'l', 'right_arrow'),
+          shell('s', 'm', 'right_arrow', 'left_command'),
         ].flatten,
   )
 end
